8cf5d8c1472734a2bb058d33179b806230a66ed8
Viktoras Bezaras <charnad@gmail.com>
2015-10-23 22:28:03 +0200

    Trying new way to push master changes

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 sculpin generate --env=live
-git checkout master
-git add output_live
-git commit -m "Bump"
-git subtree push --prefix output_live origin master
-git checkout -
+git subtree split --prefix output_live -b master
+git push -f origin master:master
+git branch -D master
