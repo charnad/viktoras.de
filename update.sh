8c078f53ea8e1b56af5f7b2aaa239aabbc9c16f6
Viktoras Bezaras <charnad@gmail.com>
2016-01-16 01:50:31 +0100

    New update script

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
+git checkout source
+rm -r output_live/*
+mkdir output_live
+git read-tree --prefix=output_live/ -u origin/master
+git reset HEAD
+git subtree pull --prefix=output_live origin master
 sculpin generate --env=live
-git subtree split --prefix output_live -b master
-git push -f origin master:master
-git branch -D master
+git add output_live
+git commit -m "Bump"
+git subtree push --prefix=output_live origin master
+git reset --hard origin/source
