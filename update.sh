16d0ee5a26c95bd3e2c105ad8de72384b94174d9
Viktoras Bezaras <charnad@gmail.com>
2018-03-27 00:38:33 +0200

    Updated update script

--- update.sh
+++ update.sh
@@ -1,11 +1,8 @@
 git checkout source
-rm -r output_live/*
-mkdir output_live
-git read-tree --prefix=output_live/ -u origin/master
-git reset HEAD
-git subtree pull --prefix=output_live origin master
+rm -r output_live
+git subtree add --prefix output_live origin master --squash
 sculpin generate --env=live
-git add output_live
+git add -f output_live
 git commit -m "Bump"
 git subtree push --prefix=output_live origin master
 git reset --hard origin/source
